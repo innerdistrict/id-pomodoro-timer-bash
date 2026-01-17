15a27dc84489799bac5d16a2ab537b064cf8d265
Greg <greg@innerdistrict.co>
2026-01-17 13:56:38 -0500

    move timer process into function

--- id-pomodoro_timer.sh
+++ id-pomodoro_timer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Colors
+# colors
 RED='\033[91m'
 GREEN='\033[92m'
 YELLOW='\033[93m'
@@ -8,12 +8,12 @@ BLUE='\033[94m'
 WHITE='\033[97m'
 ENDC='\033[0m'
 
-# Default values
+# default timer values
 WORK_MINUTES=25
 BREAK_MINUTES=5
 LONG_BREAK_MINUTES=15
 
-# Parse command line arguments
+# parse command line arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
         -w|--work)
@@ -43,7 +43,7 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# Convert minutes to seconds
+# convert minutes to seconds
 WORK_DURATION=$((WORK_MINUTES * 60))
 BREAK_DURATION=$((BREAK_MINUTES * 60))
 LONG_BREAK_DURATION=$((LONG_BREAK_MINUTES * 60))
@@ -70,7 +70,25 @@ countdown() {
     fi
 }
 
-# Clear the screen
+timer() {
+  while true; do
+    sessions_completed=$((sessions_completed + 1))
+
+    echo -e "\n${BLUE}:: Session ${sessions_completed}\n"
+    countdown $WORK_DURATION "Work"
+
+    if (( sessions_completed % 4 == 0 )); then
+      echo "Taking a long break..."
+      countdown $LONG_BREAK_DURATION "Long Break"
+    else
+      echo "Taking a short break..."
+      countdown $BREAK_DURATION "Short Break"
+    fi
+  done
+      
+}
+
+# clear the screen
 clear
 
 echo -e "${WHITE}id Pomodoro Timer Has Started! Press Ctrl+C to stop.${ENDC}"
@@ -78,17 +96,7 @@ echo -e "${WHITE}id Pomodoro Timer Has Started! Press Ctrl+C to stop.${ENDC}"
 # Trap Ctrl+C to handle cleanup
 trap 'echo -e "\n\n${WHITE}id Pomodoro session ended. Completed $((sessions_completed-1)) work sessions.${ENDC}"; exit 0' INT
 
-while true; do
-    sessions_completed=$((sessions_completed + 1))
-    
-    echo -e "\n${BLUE}:: Session ${sessions_completed}\n"
-    countdown $WORK_DURATION "Work"
-    
-    if (( sessions_completed % 4 == 0 )); then
-        echo "Taking a long break..."
-        countdown $LONG_BREAK_DURATION "Long Break"
-    else
-        echo "Taking a short break..."
-        countdown $BREAK_DURATION "Short Break"
-    fi
-done
+# run the timer
+#
+timer
+
